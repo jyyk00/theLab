b1052d23ed3970f91c4dfd10e6110dce29e18b5b
programmerkim <jenniferkim.businessonly@gmail.com>
2020-02-21 18:43:46 -0800

    dependencies in server.js

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-var express = require("express");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const passport = require("./config/passport");
