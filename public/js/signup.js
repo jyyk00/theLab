9ab9d871f6b336d2761df2d3771f1c7f094a76da
programmerkim <jenniferkim.businessonly@gmail.com>
2020-02-21 19:34:26 -0800

    public folder created so i can create the js file for the sign up, and login js, this commit also includes the start of the signup file

---
+++ public/js/signup.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
