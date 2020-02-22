fc8c0a2fb0bb0ad9fb4d9ce773ba1effebd99059
programmerkim <jenniferkim.businessonly@gmail.com>
2020-02-21 18:44:46 -0800

    setting up express app

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const passport = require("./config/passport");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
