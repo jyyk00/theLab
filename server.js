a98242e31e3fd2300ebb613a76b24542b2cc035e
programmerkim <jenniferkim.businessonly@gmail.com>
2020-02-21 18:45:59 -0800

    syncing data parsing

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const passport = require("./config/passport");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
